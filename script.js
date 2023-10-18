cf162227e0a3c0dcc6e98ddb67eb3d13d89fd719
Uthila Wickramage <uhwickramage@gmail.com>
2023-10-18 10:30:15 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+    strings:["Front End Developer","Back End Developer", "Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop:true
+})
